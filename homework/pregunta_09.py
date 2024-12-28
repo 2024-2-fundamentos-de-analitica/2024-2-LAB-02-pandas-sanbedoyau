693d401e2a54f46501510727d1d275aa7f03abec
sanbedoyau <sanbedoya@unal.edu.co>
2024-12-27 22:02:15 -0500

    Lab done

--- homework/pregunta_09.py
+++ homework/pregunta_09.py
@@ -1,17 +1,10 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
-"""
-
+import pandas as pd
 
 def pregunta_09():
-    """
+    '''
     Agregue el año como una columna al dataframe que contiene el archivo
-    `tbl0.tsv`.
-
-    Rta/
+    `tbl0.tsv`
+    R/=
         c0 c1  c2          c3  year
     0    0  E   1  1999-02-28  1999
     1    1  A   2  1999-10-28  1999
@@ -21,5 +14,9 @@ def pregunta_09():
     37  37  C   9  1997-07-22  1997
     38  38  E   1  1999-09-28  1999
     39  39  E   5  1998-01-26  1998
-
-    """
+    '''
+    df = pd.read_csv('files/input/tbl0.tsv',
+                     sep = '\t')
+    years = [date[0] for date in df['c3'].str.split('-').tolist()]
+    df['year'] = years
+    return df
--- homework/pregunta_13.py
+++ homework/pregunta_13.py
@@ -1,17 +1,10 @@
-"""
-Escriba el codigo que ejecute la accion solicitada en cada pregunta. Los
-datos requeridos se encuentran en los archivos `tbl0.tsv`, `tbl1.tsv` y 
-`tbl2.tsv`. En este laboratorio solo puede utilizar las funciones y 
-librerias de pandas para resolver las preguntas.
-"""
-
+import pandas as pd
 
 def pregunta_13():
-    """
+    '''
     Si la columna `c0` es la clave en los archivos `tbl0.tsv` y `tbl2.tsv`,
-    compute la suma de `tbl2.c5b` por cada valor en `tbl0.c1`.
-
-    Rta/
+    compute la suma de `tbl2.c5b` por cada valor en `tbl0.c1`
+    R/=
     c1
     A    146
     B    134
@@ -19,4 +12,11 @@ def pregunta_13():
     D    112
     E    275
     Name: c5b, dtype: int64
-    """
+    '''
+    tbl0 = pd.read_csv('files/input/tbl0.tsv',
+                       sep = '\t')
+    tbl2 = pd.read_csv('files/input/tbl2.tsv',
+                       sep = '\t')
+    R = tbl2.copy()
+    R = R.join(tbl0, on = 'c0', lsuffix='_tbl2', rsuffix='_tbl0')[['c1','c5b']]
+    return R.groupby('c1')['c5b'].sum()
